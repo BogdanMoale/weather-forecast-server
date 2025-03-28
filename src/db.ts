aec0f6ca55d5c6eec717b0db1f51f6a168562fdd
BogdanMoale <moale.bogdan.ionica@gmail.com>
2025-03-28 18:01:36 +0200

    Initialize database setup and update package.json for server configuration

---
+++ src/db.ts
@@ -0,0 +1,20 @@
+import Database from "better-sqlite3";
+
+const db = new Database("database/database.sqlite", { verbose: console.log });
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT CHECK( role IN ('admin', 'user') ) NOT NULL
+  )
+`);
+
+const insertUser = db.prepare(
+  "INSERT OR IGNORE INTO users (username, password, role) VALUES (?, ?, ?)"
+);
+insertUser.run("admin", "admin", "admin");
+insertUser.run("user", "user", "user");
+
+export default db;
